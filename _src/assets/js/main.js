a79adb739bc834cb5a37f578fd3babc791484461
Sandra Romero <romeroortiz.sandra@gmail.com>
2019-12-02 12:17:53 +0100

    add innerHTML search result

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,26 +2,50 @@
 console.log('>> Ready :)');
 
 const button = document.querySelector(".js-button");
-const series = [];
+const ulSearch = document.querySelector('.js-search-result-container');
+const ul = document.querySelector('.ul');
 
-function getServerData() {
+let series = [];
+
+
+// function getServerData() {
   
-  fetch(
-    'http://api.tvmaze.com/search/shows?q=girls'
-    )
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(serverData){
-      series = serverData.series;
-      // línea para que se adapten los datos recibidos a lo que necesito.
-      
-    })
-    .catch(function(err){
-      console.log('Error al traer los datos del servidor', err);
-    })
-  console.log(series);
+  //   fetch(
+    //     'http://api.tvmaze.com/singlesearch/shows?q=girls'
+    //     )
+    //     .then(function(response){
+      //       return response.json();
+      //     })
+      //     .then(function(serverData){
+        //       series = serverData.series;
+//       // línea para que se adapten los datos recibidos a lo que necesito.
+
+//     })
+//     .catch(function(err){
+  //       console.log('Error al traer los datos del servidor', err);
+  //     })
+  //   console.log(series);
+  //   paintSeries();
+  // }
+  
+  
+function paintSeries() {
+  
+  let htmlCode = "";
+
+// for (serie of series) {
+  htmlCode += '<li>';
+  htmlCode += '<h3>título serie<h3>';
+  htmlCode += '<div>';
+  htmlCode += '<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV">';
+  htmlCode += '</div>'
+  htmlCode += '</li>';
+  // }
+  ulSearch.innerHTML = htmlCode;
 }
+paintSeries();
+
+
     
-button.addEventListener('click', getServerData);
+// button.addEventListener('click', getServerData);
 
